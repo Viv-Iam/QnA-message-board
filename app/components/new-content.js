700f591e3dd2cdc7a51da3fddae66d658a1595cb
Viv-Iam <vivian.opondoh@gmail.com>
2017-03-31 14:31:19 +0300

    added component new-contentwhich adds a form to enter new content

--- app/components/new-content.js
+++ app/components/new-content.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewContent: false,
+  actions: {
+    contentFormShow() {
+      this.set('addNewContent', true);
+    },
+    saveContent1() {
+      var params = {
+        question: this.get("question"),
+        author: this.get("author"),
+        notes: this.get("notes"),
+      };
+      this.set('addNewContent', false);
+      this.sendAction('saveContent2', params);
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,7 +5,14 @@ model() {
   // return contents;(WHEN IT IS HARD CODED DATA)
   return this.store.findAll('content');//WHEN DATA IS IN DATABASE E.G FIREBASE
 },
+
 actions: {
+  saveContent3(params) {
+    var newContent = this.store.createRecord('content', params);
+    newContent.save();
+    this.transitionTo('index');
+  },
+
   destroyContent(content) {
     content.destroyContent();
     this.transitonTo('index');//this.transitionTo('index') is called to return index page after a rental is deleted

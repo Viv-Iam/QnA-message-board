3fa486e004aaf1d13da6e5c9674b7b13358ad688
Viv-Iam <vivian.opondoh@gmail.com>
2017-03-31 12:54:57 +0300

    added new component content-edit

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,12 @@ export default Ember.Route.extend({
 model() {
   // return contents;(WHEN IT IS HARD CODED DATA)
   return this.store.findAll('content');//WHEN DATA IS IN DATABASE E.G FIREBASE
+},
+actions: {
+  destroyContent(content) {
+    content.destroyContent();
+    this.transitonTo('index');//this.transitionTo('index') is called to return index page after a rental is deleted
+  }
 }
 });
 // table names in Firebase will be a plural model name, and the model hooks in your routes will refer to the singular model name.

038e38e2311372fb53c6496b531051808916ee57
Viv-Iam <vivian.opondoh@gmail.com>
2017-03-31 16:53:49 +0300

    added content route in order to create a dynamic segment

--- app/router.js
+++ app/router.js
@@ -9,7 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('contact');
-  this.route('content');
+  this.route('content', {path: '/content/:content_id'});//add dynamic segment(place holder that may be dynamically updated depending on which link we click to travel to this route) to our new route's entry in router
 });
 
 export default Router;

22f42acf9cb41e424cce72de70bfe7f03363a4ec
Viv-Iam <vivian.opondoh@gmail.com>
2017-04-03 00:47:23 +0300

    created one-to-many relationship between question and answers

--- app/models/content.js
+++ app/models/content.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 question: DS.attr(),
 author: DS.attr(),
-notes: DS.attr()
+notes: DS.attr(),
+responses: DS.hasMany('response', {async: true}) //adding responses to content model using hasMany because a single question will have many answers
 });
--- app/models/response.js
+++ app/models/response.js
@@ -3,4 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   answer: DS.attr(),
   author: DS.attr(),
+  content: DS.belongsTo('content', { async: true }), //adding content attribute to response model key work belongsTo because one question per many responses
 });

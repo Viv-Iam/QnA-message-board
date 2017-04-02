7d19b7ce29c6248d33957f64fcbf4bdb3647d97e
Viv-Iam <vivian.opondoh@gmail.com>
2017-04-02 23:01:45 +0300

    updated my Data Source by including my answers data

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 model() {
   // return contents;(WHEN IT IS HARD CODED DATA)
-  return this.store.findAll('content');//WHEN DATA IS IN DATABASE E.G FIREBASE
+  //RSVP.hash allows us to load multiple JavaScript promises at once a JavaScript promise is an operation that hasn't completed yet, but is expected to in the very near future
+  return Ember.RSVP.hash({
+  contents: this.store.findAll('content'),//WHEN DATA IS IN DATABASE E.G FIREBASE *this is a promise*
+  answers: this.store.findAll('answer')//*this is a promise*
+});
 },
 
 actions: {

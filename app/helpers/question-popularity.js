8561da6bfcf823984eafad2c189dfd502bc08dee
Viv-Iam <vivian.opondoh@gmail.com>
2017-04-05 09:59:36 +0300

    generated a helper=> question popularity adds a star glyphicon with to every question that gets three plus answers

---
+++ app/helpers/question-popularity.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export function questionPopularity(params/*, hash*/) {
+  var content = params[0];
+  if(content.get('responses').get('length') >= 3) {
+  return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"> </span>'); 
+  }
+}
+
+export default Ember.Helper.helper(questionPopularity);

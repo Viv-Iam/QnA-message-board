a304eba1b0e2f12d5d7e56537b774ece8529fc5e
Viv-Iam <vivian.opondoh@gmail.com>
2017-04-07 16:48:04 +0300

    modified action modified action and just had it in my service=> favorite questions and the component i was rendering in=> conten-edit

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  favoriteQuestion: Ember.inject.service(),
+  // favoriteQuestion: Ember.inject.service(),
 model() {
   // return contents;(WHEN IT IS HARD CODED DATA)
   //RSVP.hash allows us to load multiple JavaScript promises at once a JavaScript promise is an operation that hasn't completed yet, but is expected to in the very near future
@@ -16,11 +16,11 @@ actions: {
     var newContent = this.store.createRecord('content', params);
     newContent.save();
     this.transitionTo('index');
-  },
-  addToFav(content) {
-    content.addToFav();
-    this.transitionTo('index');
   }
+  // addToFav(content) {
+  //   content.addToFav();
+  //   this.transitionTo('index');
+  // }
   // saveAnswer(params) {
   //   var newAnswer = this.store.createRecord('response', params);
   //   newAnswer.save();
